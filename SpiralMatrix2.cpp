8fcdf973cf7cc45915a319a2e4d2b6caf174de0f
chashao <787178638@qq.com>
2017-08-21 20:09:33 +0800

    Leetcode 59

--- SpiralMatrix2.cpp
+++ SpiralMatrix2.cpp
@@ -3,6 +3,29 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        
+        int col = n, row = n;
+        vector<vector<int>> result(n, vector<int>(n,0));
+        if (!n) return result;
+        int k = 1;
+        int up = 0;
+        int down = n-1;
+        int left = 0;
+        int right = n-1;
+        while (up<=down && left <= right) {
+            for(int i = left; i <= right; i++) result[up][i] = k++;//每次都是尽可能输出最多的，直到行尾
+            up++;
+            for (int i = up; i <= down; i++) result[i][right] = k++;
+            right--;
+            for (int i = right; i >= left; i--) result[down][i] = k++;
+            down--;
+            for (int i = down; i >= up; i--) result[i][left] = k++;
+            left++;
+        }
+        return result;
     }
 };
+int main(){
+    Solution so;
+    displayVec2d(so.generateMatrix(0));
+    return 0;
+}
