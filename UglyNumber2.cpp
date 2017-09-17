9ce2effa0f29ba8f39ee0663eb357ab9a8e5baad
ss <787178638@qq.com>
2017-09-17 09:39:57 +0800

    Leetcode 264

--- UglyNumber2.cpp
+++ UglyNumber2.cpp
@@ -3,6 +3,17 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        
+        vector<int> dp(n+1, 0);
+        dp[0] = 1;
+        int index2 = 0;
+        int index3 = 0; 
+        int index5 = 0;
+        for (int i = 1; i <= n; i++) {
+            dp[i] = min(index2*2, index3*3, index5*5);
+            if (dp[i] == index2*2) index2++;
+            if (dp[i] == index3*3) index3++;
+            if (dp[i] == index5*5) index5++;
+        }
+        return dp[n-1];
     }
 };
