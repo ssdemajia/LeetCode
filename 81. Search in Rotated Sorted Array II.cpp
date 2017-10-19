c32d1ce7b649a9925d44688e2f089520c3bb6c3c
chashao <787178638@qq.com>
2017-10-19 15:41:17 +0800

    Leetcode 81

--- 81. Search in Rotated Sorted Array II.cpp
+++ 81. Search in Rotated Sorted Array II.cpp
@@ -3,6 +3,29 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        
+        int l = 0;
+        int r = nums.size()-1;
+        int mid = 0;
+        while(l<=r) {
+            mid = (l+r)/2;
+            if (nums[mid] == target) return true;
+            if (nums[mid] > nums[r]) {
+                if (nums[mid] < target ||  nums[r] >= target) {
+                    l = mid+1;
+                }else {
+                    r = mid-1;
+                } 
+            }else if (nums[r] == nums[mid]){
+                r--;
+            }
+            else {
+                if (nums[mid] < target && nums[r]>=target) {
+                    l = mid+1;
+                }else {
+                    r = mid-1;
+                }
+            }
+        }
+        return false;
     }
 };
