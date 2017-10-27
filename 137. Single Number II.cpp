3fbe143b37d8729b048ad06c3f68f8b43900f6a4
chashao <787178638@qq.com>
2017-10-27 20:23:17 +0800

    Leetcode 137

--- 137. Single Number II.cpp
+++ 137. Single Number II.cpp
@@ -1,13 +1,33 @@
 #include "inc.h"
 //137. Single Number II
+/* 
+
+思路是：将每一个位的数量计算出来保存，最后在循环一次将对3取余为1的位加到结果中
+*/
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-
-        for (int i = 1; i < nums.size(); i++) {
-            nums[0]&=nums[i];
+        vector<int> counts(32, 0);
+        for (int i = 0; i < nums.size(); i++) {
+            int temp = nums[i];
+            for (int i = 0; i < 32; i++) {
+                if (temp&1){
+                    counts[i]++;
+                }
+                temp>>=1;
+                if (!temp) break;
+            }
         }
-        return nums[0];
+        displayVec(counts);
+        int result = 0;
+        int temp = 1;
+        for (int i = 0; i < 32; i++) {
+            if (counts[i] %3==1){
+                result|=temp;
+            }
+            temp<<=1;
+        }
+        return result;
     }
 };
 int main() {
