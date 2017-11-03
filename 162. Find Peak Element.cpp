244b7c54bcece058c11ea168cba81589a4cd6725
chashao <787178638@qq.com>
2017-11-03 15:03:50 +0800

    add a logn algorithm

--- 162. Find Peak Element.cpp
+++ 162. Find Peak Element.cpp
@@ -2,7 +2,11 @@
 //162. Find Peak Element
 class Solution {
 public:
-    int findPeakElement(vector<int>& nums) {
+    /* 
+    思路：第二个方法是二分思想，通过判断mid是处于上升还是下降周期，如果是上升那么
+    缩小上升范围
+    */
+    int findPeakElement1(vector<int>& nums) {
         for (int i = 0; i < nums.size(); i++) {
             cout << nums[i]<<endl;
             if (i == 0 && nums[1] < nums[0]) return 0;
@@ -11,11 +15,27 @@ public:
         }
         return -1;
     }
+    int findPeakElement(vector<int>& nums) {
+        int start = 0;
+        int end = nums.size()-1;
+        while (start < end) {
+            cout << start << ", "<<end<<endl;
+            int mid = (start+end)/2;
+            if (nums[mid] > nums[mid+1]) {
+                end = mid;
+            }else{
+                start = mid+1;
+            }
+            // if (nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1])
+            //     return mid;
+        }
+        return start;
+    }
 };
 int main() {
     Solution so;
-    vector<int> nums = {1,2,3,4,1,2,1,2,4};
+    vector<int> nums = {1,2,3,1};
     vector<int> nums1 = {-2147483648};
-    cout << so.findPeakElement(nums1) << endl;
+    cout << so.findPeakElement(nums) << endl;
     return 0;
 }
